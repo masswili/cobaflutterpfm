3326d14a8f8f26ba82fb47db501bfd7580d0a6dd
masswili_ <mhwildan734@gmail.com>
2023-08-18 15:33:33 +0700

    Homepage new

--- lib/init/provider_list.dart
+++ lib/init/provider_list.dart
@@ -1,23 +1,23 @@
-import 'package:provider/single_child_widget.dart';
+// import 'package:provider/single_child_widget.dart';
 
-//import '../navigation/navigation_service.dart';
+// //import '../navigation/navigation_service.dart';
 
-class ApplicationProvider {
-  static ApplicationProvider? _instance;
-  static ApplicationProvider get instance {
-    _instance ??= ApplicationProvider._init();
-    return _instance!;
-  }
+// class ApplicationProvider {
+//   static ApplicationProvider? _instance;
+//   static ApplicationProvider get instance {
+//     _instance ??= ApplicationProvider._init();
+//     return _instance!;
+//   }
 
-  ApplicationProvider._init();
+//   ApplicationProvider._init();
 
-  List<SingleChildWidget> singleItems = [];
-  List<SingleChildWidget> dependItems = [
-    //ChangeNotifierProvider(
-    //  create: (_) => MenuController(),
-    //),
+//   List<SingleChildWidget> singleItems = [];
+//   List<SingleChildWidget> dependItems = [
+//     //ChangeNotifierProvider(
+//     //  create: (_) => MenuController(),
+//     //),
 
-//    Provider.value(value: NavigationService.instance)
-  ];
-  List<SingleChildWidget> uiChangesItems = [];
-}
+// //    Provider.value(value: NavigationService.instance)
+//   ];
+//   List<SingleChildWidget> uiChangesItems = [];
+// }
